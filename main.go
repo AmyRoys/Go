6c5e63856fd3833eb18c3a74d78e4774222728f7
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-04-03 11:49:34 +0100

    random number guessing

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
 
 func main() {
- fmt.Println("Hello, World!")
+ rand.Seed(time.Now().UnixNano())
+ randomNum := rand.Intn(100)
+
+ for{
+	var guess int 
+	fmt.Println("Enter your guess: ")
+	fmt.Scan(&guess)
+
+	if guess == randomNum{	
+		fmt.Println("You guessed it right!")
+		break
+	}else if guess > randomNum{
+		fmt.Println("Your guess is too high")
+	}else{
+		fmt.Println("Your guess is too low")
+	}
+ }
+
+
 }
